cd375cd9f3dbb2db8fd7961effd068b1f27705fd
Ashley Tonti <ashleyvtonti@gmail.com>
2019-02-17 21:58:18 -0600

    assignment 8 completed

--- src/App.js
+++ src/App.js
@@ -34,12 +34,17 @@ toggleComplete(index) {
   this.setState({ todos: todos });
 }
 
+deleteTodo(index) {
+  const todos = this.state.todos.filter((todo, todoIndex) => todoIndex !== index)
+  this.setState
+}
+
 render() {
     return (
       <div className="App">
        <ul>
        { this.state.todos.map( (todo, index) =>
-        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) }/>
        )}
        </ul>
        <form onSubmit={ (e) => this.handleSubmit(e) }>
