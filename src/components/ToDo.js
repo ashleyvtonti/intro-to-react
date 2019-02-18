3cea040b46fa92ff8843a34ecfe0afc54470f8de
Ashley Tonti <ashleyvtonti@gmail.com>
2019-02-17 21:40:18 -0600

    checkpoint 8 completed

---
+++ src/components/ToDo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{ this.props.description }</span>
+        <input type="button" value="delete" onClick={ this.props.deleteTodo }/>
+      </li>
+    );
+  }
+}
+
+export default ToDo;
